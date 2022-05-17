3e7ced7a42853a64425ca723e5fc72c719da3961
Sander Daskov <luxeon@live.com>
2022-05-17 09:43:14 +0300

    UPDATE

--- db.js
+++ db.js
@@ -9,9 +9,9 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Number of records deleted: " + result.affectedRows);
+        console.log(result.affectedRows + " record(s) updated");
     });
 });
